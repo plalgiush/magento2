829e4936adc69c879050775c3f054edbe31bdf4e
plalgiush <ilyasushko@yandex.ru>
2021-04-09 10:22:47 +0300

    hello world in magento 2

---
+++ app/design/frontend/Shellpea/themeFromIlya/web/js/hello.js
@@ -0,0 +1,13 @@
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    return function (config, element) {
+        console.log(config);
+        console.log(element);
+        console.log("hello world");
+    }
+});
+
+
