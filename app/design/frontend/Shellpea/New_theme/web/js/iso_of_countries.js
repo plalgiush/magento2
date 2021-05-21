7b3ddf2a2eb714c478e264713ff56fd3df844576
plalgiush <ilyasushko@yandex.ru>
2021-05-21 14:41:43 +0300

    iso of countries

---
+++ app/design/frontend/Shellpea/New_theme/web/js/iso_of_countries.js
@@ -0,0 +1,19 @@
+define([
+    'jquery'
+], function ($) {
+  'use strict';
+
+        fetch('https://magento.test/rest/all/V1/directory/countries')
+          .then(function (response) {
+            return response.json()
+                }).then(function (data) {
+                    for (let i = 0; i < data.length; i++) {
+                        // console.log(data[i]);  // вывести каждый элемент
+                    }
+                    for (var {id: id, full_name_english: f } of data) {
+                        // console.log("- " + n + " : " + f); // вывод информации через деструктурированный метод
+                        $('.column').append("- " + id + " : " + f + '<br />');
+                    }
+                })
+
+});
