de140b5c3acfdb27b26d3997613da3cc240836c5
plalgiush <ilyasushko@yandex.ru>
2021-05-31 08:52:33 +0300

    lodash

--- app/code/Shellpea/Lodash/view/frontend/web/js/diff.js
+++ app/code/Shellpea/Lodash/view/frontend/web/js/diff.js
@@ -1,15 +1,10 @@
 define([
-    // 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js'
-    'asdzxc'
 ], function() {
-    // "use strict"
     return function() {
-        console.log(123);
 
             const a1 = ['a', 'b', 'f', 'd'];
             const a2 = ['a', 'b', 'c', 'd'];
 
             console.log(_.difference(a1, a2));
-        // console.log(_.difference([2, 1], [2, 3]));
     }
 })
